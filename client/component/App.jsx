fa6d0ad416185bf4774dc74875957f0fe8b582d5
s.arash.abbasi@gmail.com <arashabbasi@Arashs-MacBook-Pro.local>
2019-08-23 14:03:17 -0700

    a working react file added

---
+++ client/component/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div>
+        Popular Dishes
+      </div>
+    );
+  }
+}
+
+export default App;
